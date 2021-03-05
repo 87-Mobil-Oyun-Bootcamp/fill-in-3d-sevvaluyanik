5c4cbe7ab6cc48a0943aa62db6af08b79f19821b
sevvaluyanik <62900193+sevvaluyanik@users.noreply.github.com>
2021-03-05 22:18:57 +0300

    First Commit

---
+++ Fill In 3D/Assets/[Game]/Scripts/BlockPooling.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockPooling : MonoBehaviour
+{
+    [SerializeField]
+    private Sprite image;
+
+
+    [SerializeField]
+    private GameObject cube;
+
+    public Material blocksColor;
+
+    private float size = 0.1f;
+
+    private Texture2D texture2D;
+
+    private Vector3 cubePos = Vector3.zero;
+
+    private void Awake()
+    {
+        PoolBlocks();
+    }
+
+    private void PoolBlocks()
+    {
+        texture2D = image.texture;
+
+        for(int x= 0; x < 32; x++)
+        {
+            for(int y = 0; y < 32; y++)
+            {
+                Color color = texture2D.GetPixel(x, y);
+
+                if (color.a == 0) {
+                    continue;
+                }
+               
+
+               GameObject cubeObj = Instantiate(cube, new Vector3(x / 10f + transform.position.x, 0.3f, y / 10f), transform.rotation, transform);
+              //  cubeObj.transform.localPosition = cubePos;
+            
+                cubeObj.transform.localScale = Vector3.one * size;
+                cubeObj.GetComponent<Renderer>().material.color = blocksColor.color;
+             
+            }
+        }
+    }
+}
---
+++ Fill In 3D/Assets/[Game]/Scripts/Hit.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hit : MonoBehaviour
+{
+    private bool isHit = true;
+
+    public GameObject blockSpawner;
+
+    private void Awake()
+    {
+        blockSpawner = GameObject.FindGameObjectWithTag("BlockSpawner");
+   
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject obje = other.gameObject;
+        if(other.CompareTag("HitBlock") && isHit)
+        {
+            
+            isHit = false;
+            Debug.Log(obje);
+            Color color = blockSpawner.GetComponent<BlockSpawner>().blockList[obje.transform];
+            Debug.Log(color);
+            obje.GetComponent<Renderer>().material.color = color;
+            obje.tag = "Untagged";
+            Destroy(this.gameObject);
+        }
+    }
+}
